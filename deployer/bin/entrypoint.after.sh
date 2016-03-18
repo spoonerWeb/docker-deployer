3b096e09d2c89dcd68d10595baf03913fba1c286
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-03-18 12:36:51 +0100

    added stage entrypoint switch

--- deployer/bin/entrypoint.after.sh
+++ deployer/bin/entrypoint.after.sh
@@ -6,6 +6,11 @@ case ${1} in
         dep -f${2} deploy live
         exit 0
          ;;
+    dep:stage)
+        set -e
+        dep -f${2} deploy stage
+        exit 0
+         ;;
     dep:develop)
         set -e
         dep -f${2} deploy develop
