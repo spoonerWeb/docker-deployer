988f5c92f3f199bdaf06ecdc2c53dc65a940dc4b
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-08-25 09:54:35 +0200

    entrypoints use sudo over su

--- bin/entrypoint.d/deployer.sh
+++ bin/entrypoint.d/deployer.sh
@@ -3,17 +3,17 @@
 case ${1} in
     dep:live)
         set -e
-        su developer -lc "dep -f\"${2}\" deploy live"
+        HOME="/home/${WEB_USER}" sudo -u ${WEB_USER} -E -- dep -f\"${2}\" deploy live
         exit 0
          ;;
     dep:stage)
         set -e
-        su developer -lc "dep -f\"${2}\" deploy stage"
+        HOME="/home/${WEB_USER}" sudo -u ${WEB_USER} -E -- dep -f\"${2}\" deploy stage
         exit 0
          ;;
     dep:develop)
         set -e
-        su developer -lc "dep -f\"${2}\" deploy develop"
+        HOME="/home/${WEB_USER}" sudo -u ${WEB_USER} -E -- dep -f\"${2}\" deploy develop
         exit 0
          ;;
 esac
