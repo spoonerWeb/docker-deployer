46ffd37b6a951c55696c6846192767a0e7f5118f
Sascha Marcel Schmidt <mail@saschaschmidt.net>
2016-11-18 11:42:51 +0100

    simplifies entrypoint

--- bin/entrypoint.d/user/deployer.sh
+++ bin/entrypoint.d/user/deployer.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
 
-function deprecationNotice() {
-    echo "###################################################"
-    echo "### Using dep:live, dep:stage or dep:develop is"
-    echo "### deprecated and being removed soon."
-    echo "Please use `dep -fpath/to/deploy.php deploy target`"
-    echo "###################################################"
-}
-
-case ${1} in
-    dep:live)
-        set -e
-        dep -f\"${2}\" deploy live
-        deprecationNotice
-        exit 0
-         ;;
-    dep:stage)
-        set -e
-        dep -f\"${2}\" deploy stage
-        deprecationNotice
-        exit 0
-         ;;
-    dep:develop)
-        set -e
-        dep -f\"${2}\" deploy develop
-        deprecationNotice
-        exit 0
-         ;;
-esac
+dep ${@}
+exit 0
